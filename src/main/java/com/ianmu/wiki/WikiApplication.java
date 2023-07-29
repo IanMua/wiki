c1b872bd84ca00c5e5356c7e02619d88c5bc10da
ianmu <ianmua.im@gmail.com>
2023-07-29 15:17:19 +0800

    修改 启动添加提示信息

--- src/main/java/com/ianmu/wiki/WikiApplication.java
+++ src/main/java/com/ianmu/wiki/WikiApplication.java
@@ -1,13 +1,19 @@
 package com.ianmu.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WikiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(WikiApplication.class, args);
+
+        LOG.info("启动成功");
     }
 
 }
