de8555dcb701bb91fb1e27b3bb6be1dec6d0e95f
ianmu <ianmua.im@gmail.com>
2023-07-29 20:44:33 +0800

    修改 mybatis依赖、配置

--- src/main/java/com/ianmu/wiki/WikiApplication.java
+++ src/main/java/com/ianmu/wiki/WikiApplication.java
@@ -1,11 +1,13 @@
 package com.ianmu.wiki;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ianmu.*")
 public class WikiApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
