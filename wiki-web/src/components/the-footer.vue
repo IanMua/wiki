8c2d072a15938edaec1d218c2faf727573c79a4a
ianmu <ianmua.im@gmail.com>
2023-07-30 15:29:02 +0800

    添加 footer组件

---
+++ wiki-web/src/components/the-footer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {defineComponent} from "vue";
+
+defineComponent({
+  name: "the-footer"
+})
+</script>
+
+<template>
+  <a-layout-footer style="text-align: center">
+    IanMu Ebook ©2023 Created by IanMu
+  </a-layout-footer>
+</template>
+
+<style scoped>
+
+</style>
