c227dd7fa9e15ec445eeeae5b66d3283a5454e50
ianmu <ianmua.im@gmail.com>
2023-07-30 19:36:59 +0800

    添加 antd icons集成

--- wiki-web/src/main.ts
+++ wiki-web/src/main.ts
@@ -1,8 +1,15 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css';
+import * as Icons from '@ant-design/icons-vue'
 
-createApp(App).use(store).use(router).use(antd).mount('#app')
+const app: any = createApp(App)
+app.use(store).use(router).use(antd).mount('#app');
+
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
