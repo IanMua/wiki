6a364444c4824bde8f95730bb6e0f6a7ab9a57b4
ianmu <ianmua.im@gmail.com>
2023-07-30 15:21:41 +0800

    添加 header组件

---
+++ wiki-web/src/components/the-header.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+import {defineComponent} from "vue";
+
+defineComponent({
+  name: 'the-header'
+})
+</script>
+
+<template>
+  <a-layout-header class="header">
+    <div class="logo"/>
+    <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 1</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<style scoped>
+
+</style>
