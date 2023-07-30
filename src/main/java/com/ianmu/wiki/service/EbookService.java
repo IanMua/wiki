7316abfe51c4aebaf99d4881e0ce815a5fc87706
ianmu <ianmua.im@gmail.com>
2023-07-30 12:09:18 +0800

    修改 EbookService list

--- src/main/java/com/ianmu/wiki/service/EbookService.java
+++ src/main/java/com/ianmu/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.ianmu.wiki.entity.EbookExample;
 import com.ianmu.wiki.mapper.EbookMapper;
 import com.ianmu.wiki.req.EbookReq;
 import com.ianmu.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.ianmu.wiki.utils.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,12 +23,6 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+        return CopyUtil.copyList(ebookList, EbookResp.class);
     }
 }
