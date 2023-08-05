deb07f3d1d9fbf050fca6b608a4047df66431bd0
ianmu <ianmua.im@gmail.com>
2023-08-05 21:09:06 +0800

    添加 后端异常统一处理

---
+++ src/main/java/com/ianmu/wiki/controller/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.ianmu.wiki.controller;
+
+import com.ianmu.wiki.resp.CommonResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 异常统一处理、预处理
+ */
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+    @ExceptionHandler(BindException.class)
+    @ResponseBody
+    public CommonResp validExceptionHandler(BindException e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.warn("参数校验失败 : {}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return commonResp;
+    }
+}
