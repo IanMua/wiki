db72b41dcf27f5dc2e08b8267f0db1e08fea6ac7
ianmu <ianmua.im@gmail.com>
2023-08-15 22:36:44 +0800

    添加md5加密传输

--- src/main/java/com/ianmu/wiki/req/WikiUserSaveReq.java
+++ src/main/java/com/ianmu/wiki/req/WikiUserSaveReq.java
@@ -2,7 +2,7 @@ package com.ianmu.wiki.req;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -16,6 +16,6 @@ public class WikiUserSaveReq {
     private String name;
 
     @NotNull(message = "密码不能为空")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[._~!@#$^&*])[A-Za-z0-9._~!@#$^&*]{6,20}$", message = "密码至少包含数字、英文和特殊符号，长度6-20")
+    @Size(min = 32, max = 32, message = "密码长度错误")
     private String password;
 }
