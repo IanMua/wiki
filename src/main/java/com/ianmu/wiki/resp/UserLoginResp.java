dc6d5ee632aafe956c623c8697a9fe8907ad884e
Ian <darrenianmu@outlook.com>
2023-08-19 23:03:31 +0800

    修改 后端 集成redis，将token和登录信息写入

--- src/main/java/com/ianmu/wiki/resp/UserLoginResp.java
+++ src/main/java/com/ianmu/wiki/resp/UserLoginResp.java
@@ -1,11 +1,14 @@
 package com.ianmu.wiki.resp;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.Serializers;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class UserLoginResp {
+public class UserLoginResp implements Serializable {
     @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
