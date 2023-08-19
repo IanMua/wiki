81e5381e86108f679f73ca6ddbd2147d4b13509a
Ian <darrenianmu@outlook.com>
2023-08-19 20:53:51 +0800

    页面添加登录功能

--- wiki-web/src/components/the-header.vue
+++ wiki-web/src/components/the-header.vue
@@ -1,15 +1,38 @@
-<script setup lang="ts">
+<script setup>
 
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
+import axios from "axios";
 
 defineComponent({
   name: 'the-header'
 })
+
+//登录模态框是否显示
+const loginModelShow = ref(false);
+//登录是否成功
+const loginResult = ref(false);
+//登录表单数据
+const loginFromData = ref({
+  loginName: "",
+  password: ""
+});
+//点击header登录按钮
+const handleClickHeaderLogin = () => {
+  loginModelShow.value = true;
+}
+//点击表单登录按钮，发送登录请求
+const handleClickLoginButton = () => {
+  axios.post("/user/login", {
+    ...loginFromData.value
+  }).then(res => {
+    console.log(res);
+  })
+}
 </script>
 
 <template>
   <a-layout-header class="header">
-    <div class="logo"/>
+    <!--    <div class="logo"/>-->
     <a-menu
         theme="dark"
         mode="horizontal"
@@ -31,9 +54,48 @@ defineComponent({
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
     </a-menu>
+    <div class="login-menu" @click="handleClickHeaderLogin">
+      <span>登录</span>
+    </div>
+    <a-modal
+        v-model:open="loginModelShow"
+        :confirm-loading="loginResult"
+        @ok="handleClickLoginButton"
+        title="登录"
+        ok-text="登录"
+        cancel-text="取消">
+      <a-form>
+        <a-form-item
+            label="用户名"
+            name="loginName"
+            :rules="[{ required: true, message: '请输入用户名' }]"
+        >
+          <a-input v-model:value="loginFromData.loginName"/>
+        </a-form-item>
+        <a-form-item
+            label="密码"
+            name="password"
+            :rules="[{ required: true, message: '请输入密码' }]"
+        >
+          <a-input v-model:value="loginFromData.password"/>
+        </a-form-item>
+      </a-form>
+    </a-modal>
   </a-layout-header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 
+.header {
+  display: flex;
+  justify-content: space-between;
+
+  .login-menu {
+    color: #ffffff;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
 </style>
