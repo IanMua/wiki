d7c2638e8a490c1013293ef432289933810fc1c1
Ian <darrenianmu@outlook.com>
2023-08-19 22:03:00 +0800

    修改 前端 用户登录校验

--- wiki-web/src/components/the-header.vue
+++ wiki-web/src/components/the-header.vue
@@ -2,6 +2,8 @@
 
 import {defineComponent, ref} from "vue";
 import axios from "axios";
+import {Md5} from "ts-md5";
+import {notification} from "ant-design-vue";
 
 defineComponent({
   name: 'the-header'
@@ -11,8 +13,14 @@ defineComponent({
 const loginModelShow = ref(false);
 //登录是否成功
 const loginResult = ref(false);
+const loginFormRef = ref();
 //登录表单数据
-const loginFromData = ref({
+const loginFormData = ref({
+  loginName: "",
+  password: ""
+});
+//登录表单提交的数据
+const loginFormSubmitData = ref({
   loginName: "",
   password: ""
 });
@@ -21,13 +29,47 @@ const handleClickHeaderLogin = () => {
   loginModelShow.value = true;
 }
 //点击表单登录按钮，发送登录请求
-const handleClickLoginButton = () => {
+const handleClickLoginButton = async () => {
+  let ok = false;
+  await loginFormRef.value.validate()
+      .then(() => {
+        ok = true;
+      })
+      .catch(error => {
+        ok = false;
+      })
+  if (!ok) {
+    return;
+  }
+
+  loginFormSubmitData.value = {
+    loginName: loginFormData.value.loginName,
+    password: Md5.hashStr(loginFormData.value.password + process.env.VUE_APP_KEY)
+  }
   axios.post("/user/login", {
-    ...loginFromData.value
+    ...loginFormSubmitData.value
   }).then(res => {
-    console.log(res);
+    if (!res.data.success) {
+      return;
+    }
+    notification["success"]({
+      message: '登录成功',
+      description:
+          '登录时间：' + new Date(),
+    });
   })
 }
+
+const rules = {
+  loginName: [
+    {required: true, message: '请输入登录名', trigger: 'change'},
+    {min: 6, max: 20, message: '长度需在6-20之间', trigger: 'blur'},
+  ],
+  password: [
+    {required: true, message: '请输入密码', trigger: 'change'},
+    {min: 6, max: 20, message: '长度需在6-20之间', trigger: 'blur'},
+  ]
+};
 </script>
 
 <template>
@@ -64,20 +106,33 @@ const handleClickLoginButton = () => {
         title="登录"
         ok-text="登录"
         cancel-text="取消">
-      <a-form>
+      <a-form
+          ref="loginFormRef"
+          :model="loginFormData"
+          :rules="rules"
+          :label-col="{
+            span: 4
+          }"
+      >
         <a-form-item
             label="用户名"
             name="loginName"
-            :rules="[{ required: true, message: '请输入用户名' }]"
         >
-          <a-input v-model:value="loginFromData.loginName"/>
+          <a-input v-model:value="loginFormData.loginName">
+            <template #prefix>
+              <UserOutlined class="site-form-item-icon"/>
+            </template>
+          </a-input>
         </a-form-item>
         <a-form-item
             label="密码"
             name="password"
-            :rules="[{ required: true, message: '请输入密码' }]"
         >
-          <a-input v-model:value="loginFromData.password"/>
+          <a-input-password v-model:value="loginFormData.password">
+            <template #prefix>
+              <LockOutlined class="site-form-item-icon"/>
+            </template>
+          </a-input-password>
         </a-form-item>
       </a-form>
     </a-modal>
